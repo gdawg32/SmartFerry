0413a9284c663fca9f557965c3c287b91cf2235b
gauresh <gnroll98@gmail.com>
2023-04-06 18:17:54 +0530

    dashboard features

--- home/views.py
+++ home/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
 from .forms import TicketForm
+import requests
 
 
 
@@ -43,4 +44,27 @@ def ticket(request):
     return render(request, 'ticket.html', context)
 
 def dashboard(request):
-    return render(request, 'dashboard.html')
+    weather = get_json()
+    context = {
+        'weather': weather['weather'][0]['main'],
+        'description': weather['weather'][0]['description'].capitalize(),
+        'visibility': weather['visibility'],
+        'temp': round(int(weather['main']['temp'])),
+
+        'pressure': weather['main']['pressure'],
+        'humidity': weather['main']['humidity'],
+        'sea_level': weather['main']['sea_level'],
+        'wind': weather['wind']['speed'],
+    }
+    return render(request, 'dashboard.html', context)
+
+
+def get_json():
+    API_KEY = "cd73fce3c747985dfb804f5efab52268"
+    ZIP = 686141
+    COUNTRY = "IN"
+    url = f"https://api.openweathermap.org/data/2.5/weather?zip={ZIP},{COUNTRY}&appid={API_KEY}&units=metric"
+    response = requests.get(url)
+    # Store the data from API
+    json_data = response.json()
+    return json_data
