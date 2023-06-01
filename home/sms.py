4f95cb11c2be07a53dc80639f171405dd928f67b
Gauresh Palakal <108043637+gdawg32@users.noreply.github.com>
2023-06-01 11:04:59 +0530

    Update sms.py

--- home/sms.py
+++ home/sms.py
@@ -1,9 +1,9 @@
 from twilio.rest import Client
 
-SID = 'AC9d6fc6ca2d7f00474fc2dc1d798920cd'
-AUTH_TOKEN = '6329b1c492fdbefaf6e1b9fba9858780'
+SID = 'AC28f23440c235e7137603171a1b4f0dd2'
+AUTH_TOKEN = '0f64e73feecae6354981e75709625090'
 
 cl = Client(SID, AUTH_TOKEN)
 
 def send_sms():
-    cl.messages.create(body='BOAT ENGINE TEMPERARTURE UNSTABLE. SEND RESCUE TEAM', from_='+15075160910', to='+918308621345')
+    cl.messages.create(body='BOAT ENGINE TEMPERARTURE UNSTABLE. SEND RESCUE TEAM', from_='+13612735622', to='+918308621345')
